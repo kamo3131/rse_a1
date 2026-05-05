bf970d68dcac9e5555b4c4dcde2ad91f65c7d3ea
Kamo3131 <kacpermar2004@gmail.com>
2026-05-05 11:38:05 +0200

    Define gravitational constant as G_CONST

--- src/gravity.py
+++ src/gravity.py
@@ -2,14 +2,15 @@ import pint
 from numba import njit
 
 si = pint.UnitRegistry()
-
+G_CONST = 6.67430e-11
 @njit
 def _calc_gravity(g_const, m1, m2, distance):
+    """Returns the value of gravity without pint units."""
     return g_const * m1 * m2 / distance**2
 
 def get_gravity(m1, m2, distance):
+    """Returns gravity value with Units."""
     m1_val = m1.to(si.kg).magnitude
     m2_val = m2.to(si.kg).magnitude
     d_val = distance.to(si.meter).magnitude
-    G = si.Quantity(1, si.gravitational_constant).to_base_units().magnitude
-    return _calc_gravity(G, m1_val, m2_val, d_val) * si.newton
+    return _calc_gravity(G_CONST, m1_val, m2_val, d_val) * si.newton
